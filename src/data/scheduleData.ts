e879a9ef4124b9780891a130a19c4c0d17b56ff7
AJ <139691145+AjR2@users.noreply.github.com>
2025-04-06 13:06:44 -0400

    Changed schedule data

--- src/data/scheduleData.ts
+++ src/data/scheduleData.ts
@@ -5,100 +5,108 @@ export const scheduleData: { [key: string]: Class[] } = {
   "Monday": [
     {
       id: "1",
-      name: "Beginner Kung Fu",
-      time: "6:00pm - 7:30pm",
+      name: "All Levels",
+      time: "5:00pm - 6:00pm",
       day: "Monday",
-      instructor: "Shifu Chen",
-      level: "Beginner"
-    },
-    {
-      id: "2",
-      name: "Advanced Forms",
-      time: "7:30pm - 9:00pm",
-      day: "Monday",
-      instructor: "Shifu Wang",
-      level: "Advanced"
+      instructor: "Shifu",
+      level: "All Levels"
     }
   ],
   "Tuesday": [
     {
       id: "3",
-      name: "Tai Chi",
-      time: "6:00pm - 7:00pm",
+      name: "All Levels",
+      time: "5:00pm - 6:00pm",
       day: "Tuesday",
-      instructor: "Shifu Li",
+      instructor: "Shifu",
       level: "All Levels"
     },
     {
       id: "4",
-      name: "Weapons Training",
-      time: "7:15pm - 8:45pm",
+      name: "All Levels",
+      time: "6:00pm - 7:00pm",
       day: "Tuesday",
-      instructor: "Shifu Zhang",
-      level: "Intermediate"
-    }
+      instructor: "Shifu",
+      level: "All Levels"
+    },
+    {
+      id: "2",
+      name: "Shaolin Wellness",
+      time: "7:00pm - 8:00pm",
+      day: "Tuesday",
+      instructor: "Shifu",
+      level: "All Levels"
+    },
   ],
   "Wednesday": [
     {
       id: "5",
-      name: "Beginner Kung Fu",
-      time: "6:00pm - 7:30pm",
+      name: "All Levels",
+      time: "5:00pm - 6:00pm",
       day: "Wednesday",
-      instructor: "Shifu Chen",
-      level: "Beginner"
+      instructor: "Shifu",
+      level: "All Levels"
     },
     {
       id: "6",
-      name: "Meditation & Qigong",
-      time: "7:45pm - 8:45pm",
+      name: "All Levels",
+      time: "6:00pm - 7:00pm",
       day: "Wednesday",
-      instructor: "Shifu Wu",
+      instructor: "Shifu",
       level: "All Levels"
     }
   ],
   "Thursday": [
     {
       id: "7",
-      name: "Sanda (Kickboxing)",
-      time: "6:00pm - 7:15pm",
+      name: "All Levels",
+      time: "5:00pm - 6:00pm",
       day: "Thursday",
-      instructor: "Shifu Liu",
-      level: "Intermediate"
+      instructor: "Shifu",
+      level: "All Levels"
     },
     {
       id: "8",
-      name: "Advanced Forms",
-      time: "7:30pm - 9:00pm",
+      name: "Shaolin Sparring",
+      time: "6:00pm - 7:00pm",
       day: "Thursday",
-      instructor: "Shifu Wang",
-      level: "Advanced"
-    }
+      instructor: "Shifu",
+      level: "All Levels"
+    },
+    {
+    id: "9",
+    name: "All Levels",
+    time: "7:00pm - 8:00pm",
+    day: "Thursday",
+    instructor: "Shifu",
+    level: "All Levels"
+  }
   ],
   "Friday": [
     {
-      id: "9",
-      name: "Open Training",
-      time: "6:00pm - 8:00pm",
+      id: "10",
+      name: "All Levels",
+      time: "5:00pm - 6:00pm",
       day: "Friday",
-      instructor: "Various Instructors",
+      instructor: "Shifu",
       level: "All Levels"
     }
   ],
-  "Saturday": [
-    {
-      id: "10",
-      name: "Kids Kung Fu",
-      time: "10:00am - 11:00am",
-      day: "Saturday",
-      instructor: "Shifu Chen",
-      level: "Kids (6-12)"
-    },
+  "Sunday": [
     {
       id: "11",
-      name: "All Levels Kung Fu",
-      time: "11:15am - 12:45pm",
+      name: "All Levels",
+      time: "10:00am - 11:00am",
       day: "Saturday",
-      instructor: "Shifu Wang",
+      instructor: "Shifu",
+      level: "All Levels"
+    },
+    {
+      id: "12",
+      name: "All Levels",
+      time: "11:00am - 12:00pm",
+      day: "Saturday",
+      instructor: "Shifu",
       level: "All Levels"
     }
   ]
